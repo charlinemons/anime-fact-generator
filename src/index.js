bdfefe04e6b1a7823c390b2d794765be67cc09f7
Charline Mons <charline.mons@gmail.com>
2024-01-23 16:10:15 -0500

    Improve the User experience

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 function displayFact(response) {
   new Typewriter("#fact", {
-    strings: [response.data.answer],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
-    cursor: "",
+    // cursor: "",
   });
 }
 
 function generateFact(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "1fabbbt6e694149ea2da3obbe200ebf2";
-  let prompt = `User instructions: Generate one random fact about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate one rare random fact about ${instructionsInput.value}`;
   let context =
-    "You mission is to generate one rare fact about the user instruction anime in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the fact.";
+    "You mission is to generate one rare fact about the user instruction anime. Make sure to follow the user instructions. Do not include a title to the fact.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let factElement = document.querySelector("#fact");
+  factElement.classList.remove("hidden");
+  factElement.innerHTML = `<div class="generating">⏳ Generating a random fact about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayFact);
 }
