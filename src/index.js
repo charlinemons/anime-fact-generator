27c55526ab6580383c9f77da6a09725a6208367c
Charline Mons <charline.mons@gmail.com>
2024-01-23 15:57:57 -0500

    Added the AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateFact(event) {
-  event.preventDefault();
-
+function displayFact(response) {
   new Typewriter("#fact", {
-    strings: "La tombe dit à la rose",
+    strings: [response.data.answer],
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateFact(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "1fabbbt6e694149ea2da3obbe200ebf2";
+  let prompt = `User instructions: Generate one random fact about ${instructionsInput.value}`;
+  let context =
+    "You mission is to generate one rare fact about the user instruction anime in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the fact.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayFact);
 }
 
 let factFormElement = document.querySelector("#fact-generator-form");
